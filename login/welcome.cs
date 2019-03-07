e8c223fcb26280981c7f96f545d03bc365221e55
mirasrobert <mrmirasrobert@gmail.com>
2019-03-07 17:45:52 +0800

    GG

---
+++ login/welcome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace login
+{
+    public partial class welcome : Form
+    {
+        public welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 x = new Form1();
+            this.Hide();
+            x.ShowDialog();
+            this.Close();
+        }
+    }
+}
